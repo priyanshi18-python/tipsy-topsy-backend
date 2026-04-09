8418bbb418c848898693386eddb184b55c859efc
stutichaurasiya <stutichaurasiya77@gamil.com>
2026-04-09 14:07:19 +0530

    working on validation of add product in services file

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -2,6 +2,24 @@ import * as productRepo from "../repositories/product.repo.js";
 
 export const createProductService = async (data) =>{
     if (!data.name || !data.price || !data.stock){
-        throw new error("")
+        throw new Error("required field missing");
+
+
     }
+
+    if(data.price <= 0){
+        throw new Error("price must be greater than 0");
+
+    }
+
+    if(data.stock < 0){
+        throw new Error("stock can not be negative");
+    }
+
+    if (!Array.isArray(data.images) || data.images.length === 0){
+        throw new Error("atleast one image is required");
+    }
+
+    const product = await productRepo.createproduct(data);
+    return product;
 }
