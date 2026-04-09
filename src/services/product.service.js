e6c23b24792da55404edc850518977bc6817c17c
priyanshi18-python <priyanshisi2424@gmail.com>
2026-04-09 23:55:37 +0530

    No changes

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -2,6 +2,6 @@ import * as productRepo from "../repositories/product.repo.js";
 
 export const createProductService = async (data) =>{
     if (!data.name || !data.price || !data.stock){
-        throw new error("")
+        throw new error("");
     }
 }
