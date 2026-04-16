c07d992a9e63610ace9f7f959082a743d277146e
stutichaurasiya <stutichaurasiya77@gamil.com>
2026-04-16 23:55:13 +0530

    changes app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ import categoryRoutes from "./routes/category.routes.js";
 const app = express(); // ✅ IMPORTANT
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001',
+  credentials:true,
+}));
 app.use(express.json());
 
 // routes
@@ -20,7 +23,7 @@ app.use("/api/categories", categoryRoutes);
 
 // test route
 app.get("/", (req, res) => {
-  res.send("API running 🚀");
+  res.send("backend running 🚀");
 });
 
 // export
